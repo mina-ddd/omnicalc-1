bf002c59b7c65de720f9bab27aee56da0d41e163
Mina-Okazaki <minami.okazaki@kellogg.northwestern.edu>
2020-05-09 17:59:16 +0000

    Routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/square/new", { :controller => "application", :action => "blank_square_form"})
+
   # Write your routes above this line
   # =================================
 
